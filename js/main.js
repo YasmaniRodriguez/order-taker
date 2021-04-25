f982bbf37aeece77234d526620a697d1eb8307b1
Yasmani Rodriguez <rodriguez.yasmani@hotmail.com>
2021-04-25 07:18:14 -0300

    se agregan las ventanas de store y promotion

--- js/main.js
+++ js/main.js
@@ -6,9 +6,8 @@ import { Order, OrderRow} from './order.js'
 var categories = JSON.parse(localStorage.getItem('categories'));
 var products = JSON.parse(localStorage.getItem('products'));
 
-var pagecontent = document.getElementById('pgc');
+var pagecontent = document.getElementById('pgbdy');
 var productcategory = document.getElementById('ctg');
-console.log(productcategory);
 var productcatalog = document.getElementById('pdt');
 
 for(const ctg of categories) {
@@ -39,25 +38,30 @@ for(const pdt of products) {
 
 var ctgcards = document.getElementsByClassName('category');
 var clrow = 1;
+var breadcrumbcs = document.getElementById('cs');
 
 ctgcards[0].onclick = function () {
     var ctgid = ctgcards[0].classList[clrow];
     document.getElementById(ctgid).checked = true;
+    breadcrumbcs.style.display = 'inline-block';
 }
 
 ctgcards[1].onclick = function () {
     var ctgid = ctgcards[1].classList[clrow];
     document.getElementById(ctgid).checked = true;
+    breadcrumbcs.style.display = 'inline-block';
 }
 
 ctgcards[2].onclick = function () {
     var ctgid = ctgcards[2].classList[clrow];
     document.getElementById(ctgid).checked = true;
+    breadcrumbcs.style.display = 'inline-block';
 }
 
 ctgcards[3].onclick = function () {
     var ctgid = ctgcards[3].classList[clrow];
     document.getElementById(ctgid).checked = true;
+    breadcrumbcs.style.display = 'inline-block';
 }
 
 //GESTION DE LA ORDEN DE PEDIDO//
@@ -68,135 +72,180 @@ saveSession('orders',JSON.stringify(orders));
 
 const currentorder = JSON.parse(sessionStorage.getItem('orders'));
 
-var listheader = document.getElementsByClassName('list-header'); 
-var listbody = document.getElementById('lb');
-console.log(listbody);
-var listfooter = document.getElementsByClassName('list-footer');
+var orderheader = document.getElementsByClassName('ohdr'); 
+var orderbody = document.getElementById('obdy');
+var orderfooter = document.getElementsByClassName('oftr');
 var pdtcards = document.getElementsByClassName('product');
-var shoppinglist = document.getElementById('list');
+var ordercontainer = document.getElementById('order');
 
 pdtcards[0].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[1].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[2].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[3].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[4].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[5].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[6].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[7].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[8].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[9].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[10].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[11].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[12].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[13].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[14].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[15].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[16].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[17].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[18].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
 pdtcards[19].onclick = function() {
     let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
+    orderbody.appendChild(orderrow);
     orderrow.classList.add("orderrow");
 }
 
-pdtcards[20].onclick = function() {
-    let orderrow = document.createElement('div');
-    listbody.appendChild(orderrow);
-    orderrow.classList.add("orderrow");
+//OVERLAY//
+var overlay = document.getElementById('overlay');
+var ovlystr = document.getElementById('ovlystr');
+var ovlypmt = document.getElementById('ovlypmt');
+var ovlyodr = document.getElementById('ovlyodr');
+var ovlycls = document.getElementsByClassName('overlay-close-btn');
+
+var actions = document.getElementsByClassName('action');
+
+actions[0].onclick = function() {
+    overlay.style.display='flex';
+    ovlystr.checked = true;
+}
+
+actions[1].onclick = function() {
+    overlay.style.display='flex';
+    ovlypmt.checked = true;
+}
+
+actions[2].onclick = function() {
+    overlay.style.display='flex';
+    ovlyodr.checked = true;
+}
+
+ovlycls[0].onclick = function() {
+    overlay.style.display='none';
+}
+
+ovlycls[1].onclick = function() {
+    overlay.style.display='none';
+}
+
+ovlycls[2].onclick = function() {
+    overlay.style.display='none';
+}
+
+//BREADCRUMB//
+
+var home = document.getElementById('hs');
+console.log(home);
+var allcheckctg = document.getElementsByName('ctg');
+console.log(allcheckctg);
+
+home.onclick = function() {
+    breadcrumbcs.style.display = 'none';
+    document.getElementById('ctg01').checked = false;
+    document.getElementById('ctg02').checked = false;
+    document.getElementById('ctg03').checked = false;
+    document.getElementById('ctg04').checked = false;
+
 }
